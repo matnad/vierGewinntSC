2ccf7148faaea29efddef5ac4761c37ba28162d9
matnad <matnad@gmail.com>
2018-10-29 20:25:42 +0100

    wip

--- viergewinnt.sol
+++ viergewinnt.sol
@@ -4,7 +4,9 @@ contract CoinFlip {
     address player1;
     address player2;
 
-    uint public bet;
+    uint256 public bet;
+    uint256 public maxBet = 10 ether;
+
     uint8[7][6] public grid;
     uint8 nStones;
 
@@ -14,37 +16,90 @@ contract CoinFlip {
     bool gameRunning;
 
     function join() public payable {
-        // Max bet
-        // check if player slots open and game gameRunning
-        // check if bet is at least as high as other player
-        // add player
+    // Check if game is gameRunning
+    require(
+      gameRunning == false,
+      "There is currently a game running. Try again when this game ended."
+    );
+
+    // Check if slots open, should usually not happen as game should be running
+    require(
+      player1 == address(0) || player2 == address(0),
+      "No player slots open currently."
+      );
+
+    // Check if bet is in acceptable range
+    require(
+      msg.value <= maxBet,
+      "Bet is too high."
+    );
+    require(
+      msg.value >= bet,
+      "Your bet must match the current bet of player 1."
+    );
+
+    // add player and do potential refund
+    if (player1 == address(0)) {
+      player1 = msg.sender;
+      bet = msg.value;
+    } else {
+      player2 = msg.sender;
+      uint256 refund = bet-msg.value;
+      if (refund > 0) {
+        msg.sender.transfer(refund);
+      }
+    }
+
+    // check if both players are successfully added and start game
+    require(
+      player1 != address(0) && player2 != address(0),
+      "Player could not be added."
+    );
+
+    setUpGame();
+    }
+
+    function setUpGame() private {
+        gameRunning = true;
+        player1Turn = true;
     }
 
     function setStone(uint8 _col) public {
-        // check if col is full
-        // add stone to col 
-        // check if victory condition reached
+        // check input
+        require(
+          _col >= 1 && _col <= 7,
+          "Must enter a number between 1 and 7."
+          );
+
+        // find row to drop stone
         uint8 col = _col-1;
         uint8 row = 0;
-        for (uint8 i=0; i<=6; i++) {
-            if (grid[col][i] == 0) {
-                row = i;
-                break;
-            }
+        while (row <= 5 && grid[col][row] != 0) {
+            row++;
         }
 
+        // check if column has room
+        require(
+          row <= 5,
+          "Selected column is full."
+          );
+
+        // drop correct stone
         if(player1Turn) {
-            grid[_col][row] = 1;
+            grid[col][row] = 1;
         } else {
-            grid[_col][row] = 2;
+            grid[col][row] = 2;
         }
 
+        // count stones
         nStones++;
 
+        // check if a player won
         if (nStones >= 7) {
-            checkVicotryCondition;
+            checkVicotryCondition();
         }
 
+        // pass the turn
         switchPriority();
     }
 
@@ -54,33 +109,15 @@ contract CoinFlip {
         // if a player won:
         // pay out winnings
 
-        setUpGame();
+        //resetGame();
     }
 
     function resetGame() private {
         player1 = address(0);
         player2 = address(0);
-        grid = uint[7][6]; // reset grid
+        delete(grid);
         bet = 0;
-
-    }
-
-    /*
-    function nStones() private returns(uint8){
-        uint8 sum = 0;
-        for (uint i=0; i<=7; i++) {
-            for (uint j=0; j<=6; j++) {
-                if (grid[i][j] > 0) {
-                    sum++;
-                }
-            }
-        }
-        return sum;
-    }*/
-
-    function setUpGame() private {
-        // give player 1 priority
-        player1Turn = true;
+        nStones = 0;
     }
 
     function switchPriority() private {
